bd249945eb392db321328e8495fa8eff2f805719
Henry Bley-Vroman <olets@olets.dev>
2020-05-03 15:51:38 -0400

    feat(parser): map parsed input to the needed data

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+const { exec } = require('child_process')
 const fs = require('fs')
 
 const gedcomJs = new require('gedcom-js').default
-const gedcom = fs.readFileSync("demo.ged", "utf8")
-const result = gedcomJs.parse(gedcom)
+const input = fs.readFileSync("demo.ged", "utf8")
+const parsedInput = gedcomJs.parse(input)
 
-result.individuals.forEach(individual => {
+const individuals = parsedInput.individuals.map(individual => {
   const name = Object.values(individual.names[0]).concat().join(' ')
   const parentIds = individual.parents.map(parent => parent.id).filter(id => id)
-  console.log({
+  return {
     name: name,
     id: individual.id,
     parentIds: [...parentIds]
-  })
+  }
 })
+
+console.log(individuals)
