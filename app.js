a2e3ab74245703dc3de8713fa43774cd664a2d9e
Henry Bley-Vroman <olets@olets.dev>
2020-05-03 15:50:37 -0400

    feat(parser): return id, full name, and array of parent ids

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
-const gedcomJs = new require('gedcom-js').default
 
+const gedcomJs = new require('gedcom-js').default
 const gedcom = fs.readFileSync("demo.ged", "utf8")
 const result = gedcomJs.parse(gedcom)
 
-console.log(result)
-
+result.individuals.forEach(individual => {
+  const name = Object.values(individual.names[0]).concat().join(' ')
+  const parentIds = individual.parents.map(parent => parent.id).filter(id => id)
+  console.log({
+    name: name,
+    id: individual.id,
+    parentIds: [...parentIds]
+  })
+})
