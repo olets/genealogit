197f39510579c9455d8fa2e74b98676f60f650f2
Henry Bley-Vroman <olets@olets.dev>
2020-05-24 12:04:39 -0400

    feat(files): support all files in pwd

--- app.js
+++ app.js
@@ -1,45 +1,73 @@
-const { execSync } = require('child_process');
-const { spawnSync} = require('child_process');
+const { execSync, spawnSync } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
-const prefix = 'genealogit/'
-
 const binPath = path.join(__dirname, 'bin')
 const gedcomJs = new require('gedcom-js').default
-const input = fs.readFileSync("demo.ged", "utf8")
-const parsedInput = gedcomJs.parse(input)
+const gedRegex = /\.ged$/
+const globalPrefix = 'genealogit/'
 
-const individuals = parsedInput.individuals.map(individual => {
-  const name = Object.values(individual.names[0]).concat().join(' ')
-  const parentIds = individual.parents.map(parent => parent.id).filter(id => id)
-  return {
-    name: name,
-    id: individual.id,
-    parentIds: [...parentIds]
-  }
+function files() {
+  let gedFiles = []
+
+  fs.readdirSync(__dirname).forEach(child => {
+    const isGed = child.match(gedRegex)
+
+    if (isGed) {
+      gedFiles = [...gedFiles, child]
+    }
+  })
+
+  return gedFiles
+}
+
+files().forEach(file => {
+  const fileContents = fs.readFileSync(file, "utf8")
+  const ged = gedcomJs.parse(fileContents)
+  const individuals = getIndividuals(ged)
+  const prefix=`${globalPrefix}${path.parse(file).name}/`
+
+  clean(prefix)
+  create(individuals, prefix)
+  connect(individuals, prefix)
 })
 
-// Delete all existing genealogit branches
-execSync(`bash ${binPath}/clean ${prefix}`)
+function getIndividuals(ged) {
+  return ged.individuals.map(individual => {
+    const name = Object.values(individual.names[0]).concat().join(' ')
+    const parentIds = individual.parents.map(parent => parent.id).filter(id => id)
+    return {
+      name: name,
+      id: individual.id,
+      parentIds: [...parentIds]
+    }
+  })
+}
 
-syncExec = command => {
+function syncExec(command) {
   const stdout = execSync(command)
-  const child = spawnSync(command,)
-  // console.error({'error': child.error})
-  // console.log({'stdout ': child.stdout})
-  // console.error({'stderr ': child.stderr})
+  const child = spawnSync(command)
+}
+
+// Delete all existing genealogit branches
+function clean(prefix) {
+  execSync(`bash ${binPath}/clean ${prefix}`)
 }
 
 // Create an orphan branch for each individual
-individuals.forEach(individual => {
-  syncExec(`bash ${binPath}/create "${individual.name}" ${individual.id}`)
-})
+function create(individuals, prefix) {
+  individuals.forEach(individual => {
+    syncExec(`bash ${binPath}/create "${individual.name}" ${individual.id} ${prefix}`)
+  })
+}
 
 // Connect children to their parents
-individuals.filter(individual => individual.parentIds.length)
-  .forEach(individual => {
+function connect(individuals, prefix) {
+  const individualsWithParents = individuals.filter(individual => individual.parentIds.length)
+
+  individualsWithParents.forEach(individual => {
     individualBranch = `${prefix}${individual.id}`
     parentBranches = individual.parentIds.map(id => `${prefix}${id}`).join(' ')
     syncExec(`bash ${binPath}/connect ${individualBranch} ${parentBranches}`)
-})
+  })
+}
